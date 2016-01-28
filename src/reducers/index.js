b98480ca7c16e683a6bc3973369b027b2c23d526
Stéphane Derosiaux <derste@gmail.com>
2016-01-28 01:56:31 +0100

    Add more cubes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { CUBE_CLICK } from '../actions/types.js';
 
+const flatten = (arrays) => [].concat.apply([], arrays);
+
+const SIZE = 50;
+const MARGIN = 5;
+
 const DEFAULT_SCENE_STATE = {
-  cubes: [
-    { x: 330, y: 100, z: 100, size: 100 },
-    { x: 250, y: 200, z: 100, size: 100 },
-    { x: 300, y: 300, z: 100, size: 100 }
-  ]
+  cubes: flatten(new Array(10).fill(null).map((_, i) =>
+                 new Array(10).fill(null).map((_, j) => ({ x: SIZE+j*(SIZE+MARGIN), y: SIZE+i*(SIZE+MARGIN), z: 0, size: SIZE }))))
 };
 
 export default (state = DEFAULT_SCENE_STATE, action) =>
